61372be293d714e8ed57215fa7154b1d2563c0bb
shubh <shubh@shubhams-mbp-2.fios-router.home>
2020-10-15 21:11:24 -0400

    First Commit.

---
+++ fifoqueue_CAR/src/main/java/com/example/demo/Fifoqueue_CAR.java
@@ -0,0 +1,110 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.services.rekognition.model.S3Object;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
+
+@SpringBootApplication
+public class Fifoqueue_CAR {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fifoqueue_CAR.class, args);
+		// Create a new instance of the builder with all defaults (credentials and
+		// region) set automatically.
+		// For more information, see Creating Service Clients in the AWS SDK for Java
+		// Developer Guide.
+
+		String bucket = "njit-cs-643";
+		String lab = "Car";
+		// String queueUrl =
+		// "https://sqs.us-east-1.amazonaws.com/278160756041/mynew_queue";
+
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAUBQ5MJVE2IXCY2W4",
+				"fT3b06nWat5H6jZXj7VgnwMyG0CkNPlzhDjCL+qD");
+
+		AmazonSQS sqs = AmazonSQSClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+				.withRegion(Regions.US_EAST_1).build();
+
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+				.withRegion(Regions.US_EAST_1).build();
+
+		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(awsCreds)).withRegion("us-east-1").build();
+		System.out.println("got connected");
+
+		// Create a FIFO queue
+		Map<String, String> attributes = new HashMap<String, String>();
+
+		// A FIFO queue must have the FifoQueue attribute set to True
+		attributes.put("FifoQueue", "true");
+
+
+		attributes.put("ContentBasedDeduplication", "true");
+
+	
+		CreateQueueRequest createQueueRequest = new CreateQueueRequest("MyFifo.fifo").withAttributes(attributes);
+		String myQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
+
+		// List queues
+		System.out.println("Listing all queues in your account.\n");
+		for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+			System.out.println(" QueueUrl: " + queueUrl);
+		}
+
+		ListObjectsV2Result result1 = s3Client.listObjectsV2(bucket);
+		List<S3ObjectSummary> objects = s3Client.listObjectsV2(bucket).getObjectSummaries();
+		for (S3ObjectSummary os : objects) {
+// DETECTION OF THE LABEL
+			DetectLabelsRequest request = new DetectLabelsRequest()
+					.withImage(new Image().withS3Object(new S3Object().withName(os.getKey()).withBucket(bucket)))
+					.withMaxLabels(10).withMinConfidence(90F);
+
+			DetectLabelsResult result = rekognitionClient.detectLabels(request);
+			for (Label label : result.getLabels()) {
+
+				if (label.getName().equals(lab) && label.getConfidence() > 90) {
+
+					
+					System.out.println("Sending a message to MyFifoQueue.fifo.\n");
+					SendMessageRequest sendMessageRequest = new SendMessageRequest(myQueueUrl, os.getKey());
+
+			
+					sendMessageRequest.setMessageGroupId("messageGroup1");
+
+					
+					SendMessageResult sendMessageResult = sqs.sendMessage(sendMessageRequest);
+					String sequenceNumber = sendMessageResult.getSequenceNumber();
+					String messageId = sendMessageResult.getMessageId();
+					System.out.println("SendMessage succeed with messageId " + messageId + ",sequence number "
+							+ sequenceNumber + "\n");
+					break;
+
+				}
+			}
+		}
+	}
+}
---
+++ fifoqueue_CAR/src/test/java/com/example/demo/Fifoqueue_CARTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fifoqueue_CARTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
